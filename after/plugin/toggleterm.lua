0b20b987199a742b5d7914336fef7380cb89ae3e
enzomarioaiello <enzomarioaiello@gmail.com>
2024-06-03 06:35:31 +0200

    Toggle term update

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -2,7 +2,7 @@ local tt = require("toggleterm")
 
 tt.setup({
   -- size can be a number or function which is passed the current terminal
-  size = 20,
+  size = 10,
   open_mapping = [[<c-\>]],
   hide_numbers = true, -- hide the number column in toggleterm buffers
   shade_terminals = true, -- NOTE: this option takes priority over highlights specified so if you specify Normal highlights you should set this to false
@@ -11,9 +11,9 @@ tt.setup({
   insert_mappings = true, -- whether or not the open mapping applies in insert mode
   terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
   persist_size = true,
-  direction = 'float',
-  close_on_exit = true, -- close the terminal window when the process exits
-   -- Change the default shell. Can be a string or a function returning a string
+  direction = 'horizontal',
+  close_on_exit = false, -- close the terminal window when the process exits
+  -- Change the default shell. Can be a string or a function returning a string
   shell = vim.o.shell,
   float_opts = {
     border = "curved",
@@ -22,12 +22,25 @@ tt.setup({
 })
 
 local Terminal = require("toggleterm.terminal").Terminal
-local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+
+-- Configure LazyGit terminal with floating window and size 17
+local lazygit = Terminal:new({
+  cmd = "lazygit",
+  hidden = true,
+  direction = "float",
+  float_opts = {
+    border = "curved",
+    width = 120,
+    height =  30,
+  },
+  on_exit = function (term)
+    vim.api.nvim_buf_delete(term.bufnr, { force = true })
+    end,
+})
 
 function _LAZYGIT_TOGGLE()
-    lazygit:toggle()
+  lazygit:toggle()
 end
 
 vim.api.nvim_set_keymap("n", "<leader>lg", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", { noremap = true, silent = true })
-
 
