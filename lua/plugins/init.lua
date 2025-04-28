4160f8d8a30e1f42a9fa88376d18ee9fe9cbc50a
enzomarioaiello <enzomarioaiello@gmail.com>
2025-04-28 19:05:48 +0200

    idk

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -170,27 +170,27 @@ return {
   },
 
   -- Supermaven
-  {
-    "supermaven-inc/supermaven-nvim",
-    config = function()
-      require("supermaven-nvim").setup({
-        keymaps = {
-          accept_suggestion = "<Tab>",
-          clear_suggestion = "<C-]>",
-          accept_word = "<C-k>",
-        },
-        ignore_filetypes = { cpp = true },
-        color = {
-          suggestion_color = "#a05da2",
-          cterm = 10,
-        },
-        log_level = "info",
-        disable_inline_completion = false,
-        disable_keymaps = false,
-        condition = function()
-          return false
-        end,
-      })
-    end,
-  },
-} 
+  -- {
+  --   "supermaven-inc/supermaven-nvim",
+  --   config = function()
+  --     require("supermaven-nvim").setup({
+  --       keymaps = {
+  --         accept_suggestion = "<Tab>",
+  --         clear_suggestion = "<C-]>",
+  --         accept_word = "<C-k>",
+  --       },
+  --       ignore_filetypes = { cpp = true },
+  --       color = {
+  --         suggestion_color = "#a05da2",
+  --         cterm = 10,
+  --       },
+  --       log_level = "info",
+  --       disable_inline_completion = false,
+  --       disable_keymaps = false,
+  --       condition = function()
+  --         return false
+  --       end,
+  --     })
+  --   end,
+  -- },
+}
